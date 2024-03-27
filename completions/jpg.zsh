b44850825da3499ce298579adf1930215203edb8
Meow King <mr.meowking@anche.no>
2024-03-27 12:16:41 +0800

    feat: enhance zsh completion

--- completions/jpg.zsh
+++ completions/jpg.zsh
@@ -4,99 +4,39 @@ autoload -U is-at-least
 
 _jpg() {
     typeset -A opt_args
-    typeset -a _arguments_options
-    local ret=1
+    local context state line
 
-    if is-at-least 5.2; then
-        _arguments_options=(-s -S -C)
-    else
-        _arguments_options=(-s -C)
-    fi
-
-    local context curcontext="$curcontext" state line
-
-    _arguments '1: :_jpg_commands' \
-        '*: :->args' \
-        && ret=0
+    _arguments -C \
+               '1: :->command' \
+               '*:: :->args'
 
     case $state in
+        command)
+            local jpg_list_output="$(_call_program commands jpg --list 2> /dev/null)"
+            local cmds
+            {
+                read # skip first line
+                while read -r line; do
+                    # Extract the command name
+                    local command_name=$(echo "$line" | awk '{print $1}')
+                    # Extract the description (assuming it follows a '#' character)
+                    local description=$(echo "$line" | cut -d'#' -f2- | xargs)
+                    # Combine the command name and description in the format expected by _describe
+                    cmds+=("$command_name":"$description")
+                done     
+            } <<< "$jpg_list_output"
+
+            _describe -t cmds 'jpg commands' cmds
+            ;;
         args)
-            curcontext="${curcontext%:*}-${words[2]}:"
-
-            local lastarg=${words[${#words}]}
-            local recipe
-
-            local cmds; cmds=(
-                ${(s: :)$(_call_program commands jpg --summary)}
-            )
-
-            # Find first recipe name
-            for ((i = 2; i < $#words; i++ )) do
-                if [[ ${cmds[(I)${words[i]}]} -gt 0 ]]; then
-                    recipe=${words[i]}
-                    break
-                fi
-            done
-
-            if [[ $lastarg = */* ]]; then
-                # Arguments contain slash would be recognised as a file
-                _arguments -s -S $common '*:: :_files'
-            elif [[ $lastarg = *=* ]]; then
-                # Arguments contain equal would be recognised as a variable
-                _message "value"
-            elif [[ $recipe ]]; then
-                # Show usage message
-                _message "`jpg --show $recipe`"
-                # Or complete with other commands
-                #_arguments -s -S $common '*:: :_jpg_commands'
-            else
-                _arguments -s -S $common '*:: :_jpg_commands'
-            fi
-        ;;
+            case $words[1] in
+                config)
+                    local variables; variables=("${(s. .)$(jpg --variables 2> /dev/null)}" "-a")
+                    _describe -t variables 'jpg config variables' variables
+                    ;;
+            esac
+            ;;
     esac
-
-    return ret
-}
-
-(( $+functions[_jpg_commands] )) ||
-_jpg_commands() {
-    [[ $PREFIX = -* ]] && return 1
-    integer ret=1
-    local variables; variables=(
-        ${(s: :)$(_call_program commands jpg --variables)}
-    )
-    local commands; commands=(
-        ${${${(M)"${(f)$(_call_program commands jpg --list)}":#    *}/ ##/}/ ##/:Args: }
-    )
-
-    if compset -P '*='; then
-        case "${${words[-1]%=*}#*=}" in
-            *) _message 'value' && ret=0 ;;
-        esac
-    else
-        _describe -t variables 'variables' variables -qS "=" && ret=0
-        _describe -t commands 'jpg commands' commands "$@"
-    fi
-
-}
-
-(( $+functions[_jpg_variables] )) ||
-_jpg_variables() {
-    [[ $PREFIX = -* ]] && return 1
-    integer ret=1
-    local variables; variables=(
-        ${(s: :)$(_call_program commands jpg --variables)}
-    )
-
-    if compset -P '*='; then
-        case "${${words[-1]%=*}#*=}" in
-            *) _message 'value' && ret=0 ;;
-        esac
-    else
-        _describe -t variables 'variables' variables && ret=0
-    fi
-
-    return ret
 }
 
 _jpg "$@"
